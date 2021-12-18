81c010e299ebead367c5f000066432c160f0f43c
Omar Abdullah <Malfucious97@gmail.com>
2021-12-18 16:09:14 -0600

    Added more css, h2 and h3 element was added, and updated my index.js

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const express = require ('express')
+const path = require('path/posix')
 const app = express()
 const PORT = 4040
+
+app.get('/index.js',(req,res) =>{
+    res.sendFile(path.join(__dirname, './index.js'))
+})
+
+app.use('/index.js',express.static(path.join(__dirname, 'index.js')))
 
 app.listen(PORT, () => {
     console.log(`App listening to ${PORT}`)
